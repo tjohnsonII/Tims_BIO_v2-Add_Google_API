b6c5dfe8cda201fb38087d6a982c5f7484de68db
tjohnson082083@gmail.com <tjohnson082083@gmail.com>
2023-05-13 12:03:49 -0400

    update

---
+++ submit_form.php
@@ -0,0 +1,66 @@
+<?php
+require_once __DIR__ . './client_secret_116122727779-mkp79jbsb5pr8mgtcmtt32uug3g43p9u.apps.googleusercontent.com copy.json'; // Path to the Google API PHP client library
+
+// Initialize the Google API client
+$client = new Google_Client();
+$client->setApplicationName('Your Application Name');
+$client->setScopes(Google_Service_Gmail::MAIL_GOOGLE_COM);
+$client->setAuthConfig('path/to/client_credentials.json'); // Path to your client credentials JSON file
+
+// Create a new Gmail service instance
+$service = new Google_Service_Gmail($client);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Collect form data
+  $name = sanitizeInput($_POST["name"]);
+  $email = sanitizeInput($_POST["email"]);
+  $message = sanitizeInput($_POST["message"]);
+
+  // Validate form data
+  if (empty($name) || empty($email) || empty($message)) {
+    echo "<p>Please fill in all the required fields.</p>";
+    header("Location: index.html"); // Redirect to index.html
+    exit();
+  }
+
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "<p>Please enter a valid email address.</p>";
+    header("Location: index.html"); // Redirect to index.html
+    exit();
+  }
+
+  // Prepare the email message
+  $emailSubject = "New Contact Form Submission";
+  $emailContent = "Name: $name\nEmail: $email\nMessage: $message";
+  $emailTo = "recipient@gmail.com"; // Replace with your desired recipient email address
+
+  try {
+    // Create a new Gmail message
+    $message = new Google_Service_Gmail_Message();
+    $email->setSubject($emailSubject);
+    $email->setRaw(base64_encode("To: $emailTo\r\n" .
+      "Subject: $emailSubject\r\n" .
+      "Content-Type: text/plain; charset=utf-8\r\n\r\n" .
+      $emailContent));
+
+    // Send the email
+    $service->users_messages->send("me", $message);
+
+    echo "<p>Your message has been sent successfully. Thank you!</p>";
+    header("Location: index.html"); // Redirect to index.html
+    exit();
+  } catch (Exception $e) {
+    echo "<p>Sorry, there was an error sending your message. Please try again later.</p>";
+    header("Location: index.html"); // Redirect to index.html
+    exit();
+  }
+}
+
+// Function to sanitize form input
+function sanitizeInput($input) {
+  $input = trim($input);
+  $input = stripslashes($input);
+  $input = htmlspecialchars($input);
+  return $input;
+}
+?>
